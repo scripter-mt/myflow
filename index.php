8df442c2e977b36586bfc27f6c2020048625d7f2
scripter-mt <thapamanish427@gmail.com>
2021-06-21 06:17:47 +0545

    new update

--- index.php
+++ index.php
@@ -2,11 +2,14 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>PHP-Primer</title>
+    <title>Project</title>
   </head>
   <body>
-    <h1>Hello World</h1>
-    <?php
+    <h1>Hello Urba</h1>
+    <img src="ocean.jpg" width="300" height="300">
+    <h2>Do you wanna play game</h2>
+
+
     echo "This is php file";
     echo "</br/>";
     echo "Line is breaked now";
