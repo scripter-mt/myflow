4f7346f9c0a48bcd5131cb8734eee83064aed058
scripter-mt <thapamanish427@gmail.com>
2021-06-27 08:49:14 +0545

    download

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Project</title>
+    <title>Christain Community</title>
   </head>
   <body>
-    <h1>Hello Urba</h1>
-    <img src="ocean.jpg" width="300" height="300">
+    <h1>Christain Community</h1>
+    <img src="ocean.jpg" width="300" height="400">
     <h2>Do you wanna play game</h2>
 <audio controls>
-  <source src="lagjaa.mp3" type="mp3/mpeg">
-
+  <source src="lagjaa.mp3" type="audio/mpeg">
+    <a href="lagjaa.mp3" download="download" >download</a>
 </audio>
 
-    
+
   </body>
 </html>
